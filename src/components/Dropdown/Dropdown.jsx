81e9dcca8cb5c115ca86947d9e5eead0ac4f44d9
Jakub Kobialka <kobialka.kontakt@gmail.com>
2023-08-16 11:24:28 +0200

    dropdown menu added

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Dropdown.scss"
+import { logout } from "../../services/api";
+
+export default function Dropdown({ toggleState })
+{
+    return (
+        <div className={`z-10 bg-white ${ toggleState ? "" : "hidden" } divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dropdown`}>
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profil</a>
+                </li>
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Statystyki</a>
+                </li>
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Ulubione</a>
+                </li>
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Twoje pytania</a>
+                </li>
+            </ul>
+            <div className="py-2" onClick={logout}>
+                <span className="block px-4 cursor-pointer py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Wyloguj się</span>
+            </div>
+        </div>
+
+        
+    )
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.scss'
 import { logout } from "../../services/api";
+import Dropdown from "../Dropdown/Dropdown";
 
 export default function Header()
 {
+    const [toggleMenuState, setToggleMenuState] = useState(false)
+
     function toggleMenu()
     {
         console.log("Toggle menu!")
+        setToggleMenuState(prev => !prev)
     }
 
     return (
         <header className="header flex flex-row justify-between items-center w-full p-2">
             <img className="rounded-full" src="https://placehold.co/60" alt="LEKpendium logo" />
-            <div className="user-profile flex flex-row items-center">
+            <div className="user-profile flex flex-row items-center justify-around	w-48">
                 <img className="rounded-full m-2" src="https://placehold.co/45" alt="User avatar" />
                 <div className="user-name">
                     <span className="font-semibold">Jan Nowak</span>
                 </div>
-                <button className="toggleMenu" onClick={toggleMenu}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 m-1">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                    </svg>
-                </button>
-
-                <button onClick={logout}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-                </button>
+                <div className="dropdown-menu-block">
+                    <button className="toggleMenu" onClick={toggleMenu}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </button>
+                    <Dropdown toggleState={toggleMenuState}/>
+                </div>
             </div>
         </header>
     )
